a39bd34366d9fcc3e7dbac7382a896e9269578d2
bonvit <bonarv96@gmail.com>
2017-06-10 19:44:56 +0300

    Provides AppComponent

--- app/src/main/java/com/vb/childrenmonitor/App.java
+++ app/src/main/java/com/vb/childrenmonitor/App.java
@@ -1,15 +1,38 @@
 package com.vb.childrenmonitor;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.vb.childrenmonitor.dagger.component.AppComponent;
+import com.vb.childrenmonitor.dagger.component.DaggerAppComponent;
+import com.vb.childrenmonitor.dagger.module.FirebaseModule;
 
 /**
  * Created by bonar on 6/10/2017.
  */
 
 public class App extends Application {
+    private static final String TAG = "App";
+
+    AppComponent mAppComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate");
+
+        mAppComponent = buildComponent();
+    }
+
+    public AppComponent getAppComponent() {
+        return mAppComponent;
+    }
+
+    private AppComponent buildComponent() {
+        Log.d(TAG, "buildComponent");
+
+        return DaggerAppComponent.builder()
+                .firebaseModule(new FirebaseModule())
+                .build();
     }
 }
