485b199b00245ea94edc46df9edf97004601327b
bonvit <bonarv96@gmail.com>
2017-06-10 19:19:12 +0300

    Add Application

---
+++ app/src/main/java/com/vb/childrenmonitor/App.java
@@ -0,0 +1,15 @@
+package com.vb.childrenmonitor;
+
+import android.app.Application;
+
+/**
+ * Created by bonar on 6/10/2017.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
