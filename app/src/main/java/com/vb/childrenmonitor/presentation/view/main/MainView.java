d1f8dbcdd25f2f4dcbffe9ad9dcf5346b7ab4d05
bonvit <bonarv96@gmail.com>
2017-06-12 15:39:43 +0300

    Add MainView

---
+++ app/src/main/java/com/vb/childrenmonitor/presentation/view/main/MainView.java
@@ -0,0 +1,11 @@
+package com.vb.childrenmonitor.presentation.view.main;
+
+import com.arellomobile.mvp.MvpView;
+
+/**
+ * Created by bonar on 6/11/2017.
+ */
+
+public interface MainView extends MvpView {
+    void startLoginActivity();
+}
